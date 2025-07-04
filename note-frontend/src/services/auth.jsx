308f12226f0d218f1bcca46de494b75eb0c13c5c
Swarnadeep Jana <53296273+swarnadeepjana@users.noreply.github.com>
2025-07-04 15:37:28 +0530

    Update auth.jsx

--- note-frontend/src/services/auth.jsx
+++ note-frontend/src/services/auth.jsx
@@ -1,13 +1,12 @@
-// src/services/auth.js
 import axios from "axios";
 
 const API = import.meta.env.VITE_API_BASE_URL;
 
-export const login = async (email, password) => {
-  const res = await axios.post(`${API}/auth/login`, { email, password });
+export const Login = async (email, password) => {
+  const res = await axios.post(`${API}/auth/Login`, { email, password });
   return res.data.access_token;
 };
 
-export const register = async (email, password) => {
-  return await axios.post(`${API}/auth/register`, { email, password });
+export const Register = async (email, password) => {
+  return await axios.post(`${API}/auth/Register`, { email, password });
 };
