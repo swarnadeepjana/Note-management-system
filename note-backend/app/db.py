6c5536d9a6e865bdf8bfa1cfdaa8ca1ad9e8162c
Swarnadeep Jana <53296273+swarnadeepjana@users.noreply.github.com>
2025-07-04 16:07:17 +0530

    Update db.py

--- note-backend/app/db.py
+++ note-backend/app/db.py
@@ -1,9 +1,8 @@
-# app/db.py
 import motor.motor_asyncio
 from decouple import config
 from app.core.config import settings
 
 
-MONGO_URL = config("MONGO_URL")  # reads from .env
+MONGO_URL = config("MONGO_URL") 
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL)
 db = client.get_database()  # This connects to the 'note_app' DB
