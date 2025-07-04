341d0d9c62a93ca84fa4d786a37c2e2c562923b3
Swarnadeep Jana <53296273+swarnadeepjana@users.noreply.github.com>
2025-07-04 16:09:09 +0530

    Update test_auth.py

--- note-backend/tests/test_auth.py
+++ note-backend/tests/test_auth.py
@@ -1,4 +1,3 @@
-# tests/test_auth.py
 import pytest
 from httpx import AsyncClient
 from app.main import app
@@ -7,15 +6,14 @@ from app.db import db
 @pytest.mark.asyncio
 async def test_register_and_login():
     async with AsyncClient(app=app, base_url="http://test") as ac:
-        # Register
-        response = await ac.post("/auth/register", json={
+        
+        response = await ac.post("/Auth/Register", json={
             "email": "test@example.com",
             "password": "test123"
         })
         assert response.status_code == 200
 
-        # Login
-        response = await ac.post("/auth/login", json={
+        response = await ac.post("/Auth/Login", json={
             "email": "test@example.com",
             "password": "test123"
         })
