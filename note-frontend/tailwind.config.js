ea9e50b525becf12ee22772d6d0fad9a79943f16
Swarnadeep Jana <53296273+swarnadeepjana@users.noreply.github.com>
2025-07-04 16:22:51 +0530

    Update tailwind.config.js

--- note-frontend/tailwind.config.js
+++ note-frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}", // Ensure it scans all components and pages
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
