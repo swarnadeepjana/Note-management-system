8a2eb8d009217f17b8959304d1764c5aca5aa14f
Swarnadeep Jana <53296273+swarnadeepjana@users.noreply.github.com>
2025-07-04 14:56:00 +0530

    Update main.py

--- note-backend/app/main.py
+++ note-backend/app/main.py
@@ -1,22 +1,21 @@
-# app/main.py
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes import auth, notes, share, analytics
 from app.db import db
-from app.routes import public_notes  # ✅ import it
+from app.routes import public_notes  
 
 app = FastAPI()
 
-# CORS for React frontend
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:5175"],
+    allow_origins=["https://notemanagement-system.netlify.app", "http://localhost:5173", "http://localhost:5175"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Route registration
+
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(notes.router, prefix="/notes", tags=["Notes"])
 app.include_router(share.router, prefix="/share", tags=["Share"])
