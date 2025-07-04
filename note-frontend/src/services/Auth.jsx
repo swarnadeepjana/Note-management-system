5c9e76ea7ebfda91c3a7ba72dd6d5c72775e36f0
Swarnadeep Jana <53296273+swarnadeepjana@users.noreply.github.com>
2025-07-04 15:42:21 +0530

    Update and rename auth.jsx to Auth.jsx

--- note-frontend/src/services/Auth.jsx
+++ note-frontend/src/services/Auth.jsx
@@ -3,10 +3,10 @@ import axios from "axios";
 const API = import.meta.env.VITE_API_BASE_URL;
 
 export const Login = async (email, password) => {
-  const res = await axios.post(`${API}/auth/Login`, { email, password });
+  const res = await axios.post(`${API}/Auth/Login`, { email, password });
   return res.data.access_token;
 };
 
 export const Register = async (email, password) => {
-  return await axios.post(`${API}/auth/Register`, { email, password });
+  return await axios.post(`${API}/Auth/Register`, { email, password });
 };
