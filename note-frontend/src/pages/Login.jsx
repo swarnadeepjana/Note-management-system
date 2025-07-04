19940be3d5b24b951c0fe6be33554929f6a994cc
Swarnadeep Jana <53296273+swarnadeepjana@users.noreply.github.com>
2025-07-04 15:22:40 +0530

    Update Login.jsx

--- note-frontend/src/pages/Login.jsx
+++ note-frontend/src/pages/Login.jsx
@@ -12,7 +12,7 @@ function Login() {
     e.preventDefault();
     try {
       const res = await axios.post(
-        `${import.meta.env.VITE_API_BASE_URL}/auth/login`,
+        `${import.meta.env.VITE_API_BASE_URL}/auth/Login`,
         { email, password },
         {
           headers: {
